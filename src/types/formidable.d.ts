a578d64a6348b820aeb9e91b0690cfbee039d3da
henrytatt <henryinktatt@gmail.com>
2025-09-26 10:33:19 -0500

    chore(ts): add shim types for formidable

---
+++ src/types/formidable.d.ts
@@ -0,0 +1,10 @@
+﻿declare module "formidable" {
+  export type File = {
+    filepath: string;
+    originalFilename?: string | null;
+    mimetype?: string | null;
+    size: number;
+  };
+  const formidable: any;
+  export default formidable;
+}
